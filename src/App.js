1ac82ac129d1972d6666a56e4cb3bd8f98c04ab8
RobinJessicaLittle <robin.jessica.little@outlook.com>
2022-04-19 17:40:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+// import { useEffect, useState } from 'react';
+// import './App.css';
+
+// function App() {
+// 	const [characters, setCharacters] = useState([]);
+// 	const [error, setError] = useState(null);
+
+// 	useEffect(() => {
+// 		const fetchCharacters = async () => {
+// 			try {
+// 				const response = await fetch(
+// 					'https://www.breakingbadapi.com/api/characters?limit=10&offset=10',
+// 				);
+// 				if (!response.ok) {
+// 					throw new Error(response.statusText);
+// 				}
+
+// 				const data = await response.json();
+// 				setCharacters(data);
+// 			} catch (err) {
+// 				setError('Could not fetch data');
+// 				console.log(err.message);
+// 			}
+// 		};
+// 		fetchCharacters();
+// 	}, []);
+
+// 	return (
+// 		<div className="App">
+// 			{characters.map((character, index) => (
+// 				<div key={index}>
+// 					{error && <p>{error}</p>}
+// 					<h1>{character.name}</h1>
+// 					<img src={character.img} alt="breaking bad" />
+// 				</div>
+// 			))}
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [chucknorris, setChucknorris] = useState([]);
+	const [error, setError] = useState(null);
+
+    useEffect(() => {
+		const fetchChuck = async () => {
+            try {
+				const response = await fetch(
+					'http://api.icndb.com/jokes/random');
+                if (!response.ok) {
+					throw new Error(response.statusText);
+				}
+        console.log (chucknorris)
+				const data = await response.json();
+				setChucknorris(data);
+            } catch (err) {
+				setError('Could not fetch data');
+				console.log(err.message);
+			}
+        };
+		fetchChuck();
+	}, []);
+
+    return (
+		<div className="App">
+			{chucknorris.map((chucknorris, index) => (
+				<div key={index}>
+					{error && <p>{error}</p>}
+					<h1>{chucknorris.jokes}</h1>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default App;
